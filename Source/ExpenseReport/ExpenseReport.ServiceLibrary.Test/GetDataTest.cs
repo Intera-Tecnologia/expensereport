7efef7fe5af064e6f5fc1cefa4d3d2ff3d1a7c6f
Alexander Jeske <alexander@intera.org.br>
2016-09-15 11:42:20 -0400

    Adding Basic WCF Service Adding Basic WCF Service Test

---
+++ Source/ExpenseReport/ExpenseReport.ServiceLibrary.Test/GetDataTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseReport.ServiceLibrary.Test.ServiceClient;
+using System.Linq;
+
+namespace ExpenseReport.ServiceLibrary.Test
+{
+   [TestClass]
+   public class GetDataTest
+   {
+      [TestMethod]
+      public void TestGetData()
+      {
+         ExpenseReportServiceClient wcfService = new ExpenseReportServiceClient();
+         String retorno = wcfService.GetData( 5 );
+         Assert.IsTrue( retorno.Contains( "5" ) );
+      }
+   }
+}
